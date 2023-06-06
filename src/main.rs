250a1822b332036214f095a1b7e756eadbbbb7bf
Mathieu Mercier <merciermathieu412@gmail.com>
2023-06-06 14:23:21 -0400

    random creation version

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,14 @@ const FRAME_DELAY_MS: u64 = 50;
 fn main() {
     let mut grid = create_grid();
 
+    // Set initial state with random cell values
     let mut rng = rand::thread_rng();
-    for _ in 0..(GRID_WIDTH * GRID_HEIGHT / 10) {
-        let row = rng.gen_range(0..GRID_HEIGHT);
-        let col = rng.gen_range(0..GRID_WIDTH);
-        grid[row][col] = true;
+
+    for i in 0..GRID_HEIGHT {
+        for j in 0..GRID_WIDTH {
+            let value = rng.gen_range(1.0..10.0);
+            grid[i][j] = if value > 9.0 { 1.0 } else { 0.0 }; // Set the cell value based on the condition
+        }
     }
 
     let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
@@ -41,10 +44,10 @@ fn main() {
     while window.is_open() && !window.is_key_down(Key::Escape) {
         if window.is_key_pressed(Key::R, KeyRepeat::No) {
             grid = create_grid();
-            for _ in 0..(GRID_WIDTH * GRID_HEIGHT / 10) {
-                let row = rng.gen_range(0..GRID_HEIGHT);
-                let col = rng.gen_range(0..GRID_WIDTH);
-                grid[row][col] = true;
+            for i in 0..GRID_HEIGHT {
+                for j in 0..GRID_WIDTH {
+                    grid[i][j] = rng.gen_range(0.0..1.0);
+                }
             }
             restart = true;
         }
@@ -62,15 +65,14 @@ fn main() {
     }
 }
 
-fn create_grid() -> Vec<Vec<bool>> {
-    vec![vec![false; GRID_WIDTH]; GRID_HEIGHT]
+fn create_grid() -> Vec<Vec<f64>> {
+    vec![vec![0.0; GRID_WIDTH]; GRID_HEIGHT]
 }
 
-fn print_grid(buffer: &mut Vec<u32>, grid: &Vec<Vec<bool>>) {
+fn print_grid(buffer: &mut Vec<u32>, grid: &Vec<Vec<f64>>) {
     for (i, row) in grid.iter().enumerate() {
-        for (j, &cell) in row.iter().enumerate() {
-            let alive_neighbors = count_alive_neighbors(&grid, i, j);
-            let color_value = (alive_neighbors as u32) * (255 / 9); 
+        for (j, &value) in row.iter().enumerate() {
+            let color_value = (value * 255.0) as u32; // Map cell value to grayscale value
 
             for x in 0..CELL_SIZE {
                 for y in 0..CELL_SIZE {
@@ -85,35 +87,37 @@ fn print_grid(buffer: &mut Vec<u32>, grid: &Vec<Vec<bool>>) {
     }
 }
 
-fn update_grid(grid: &mut Vec<Vec<bool>>) {
+fn update_grid(grid: &mut Vec<Vec<f64>>) {
     let mut new_grid = create_grid();
 
-    for (i, row) in grid.iter().enumerate() {
-        for (j, &cell) in row.iter().enumerate() {
-            let alive_neighbors = count_alive_neighbors(&grid, i, j);
+    for i in 0..GRID_HEIGHT {
+        for j in 0..GRID_WIDTH {
+            let value = grid[i][j];
+            let alive_neighbors = smooth_life_neighbors(&grid, i, j);
 
-            new_grid[i][j] = match (cell, alive_neighbors) {
-                (true, 2) | (true, 3) | (false, 3) => true,
-                _ => false,
-            };
+            let new_value = value + (alive_neighbors - value) * 0.05; // Apply smooth life rules
+
+            new_grid[i][j] = new_value.max(0.0).min(1.0); // Ensure value is within range [0, 1]
         }
     }
 
     *grid = new_grid;
 }
 
-fn count_alive_neighbors(grid: &Vec<Vec<bool>>, row: usize, col: usize) -> u8 {
+fn smooth_life_neighbors(grid: &Vec<Vec<f64>>, row: usize, col: usize) -> f64 {
+    let mut sum = 0.0;
     let mut count = 0;
 
     for i in (row as isize - 1)..=(row as isize + 1) {
         for j in (col as isize - 1)..=(col as isize + 1) {
             if (i >= 0) && (i < GRID_HEIGHT as isize) && (j >= 0) && (j < GRID_WIDTH as isize) {
-                if !(i == row as isize && j == col as isize) && grid[i as usize][j as usize] {
+                if !(i == row as isize && j == col as isize) {
+                    sum += grid[i as usize][j as usize];
                     count += 1;
                 }
             }
         }
     }
 
-    count
+    sum / (count as f64)
 }
